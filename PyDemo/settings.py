f881efbab5b51a463c1d82ac892e3760ad1b0889
MastergamingYT <abdullahmohamedezzat21@gmail.com>
2021-08-04 22:02:33 +0200

    PyCommerce V4

--- PyDemo/settings.py
+++ PyDemo/settings.py
@@ -26,11 +26,13 @@ SECRET_KEY = 'django-insecure-x2s7sdu)nsk=9xab9t2ylho5%n%0nvbme8p=2&dl!6$3ny!&mh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://angularpycommerce.herokuapp.com',
+ALLOWED_HOSTS = ['pycommerceapp.herokuapp.com',
+                 'https://angularpycommerce.herokuapp.com',
                  'https://pycommerceapp.netlify.app',
                  'https://pycommerceapp.web.app', ]
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
+    'pycommerceapp.herokuapp.com',
     'https://angularpycommerce.herokuapp.com',
     'https://pycommerceapp.netlify.app',
     'https://pycommerceapp.web.app',
